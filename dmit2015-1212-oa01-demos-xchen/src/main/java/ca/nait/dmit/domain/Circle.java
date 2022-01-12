7b520b3649beab274ecd1a0f7665d9b3b7791bf2
Xiao Chen <cxcanada@hotmail.com>
2022-01-12 11:47:32 -0700

    end of Jan 12 class code along

---
+++ dmit2015-1212-oa01-demos-xchen/src/main/java/ca/nait/dmit/domain/Circle.java
@@ -0,0 +1,24 @@
+package ca.nait.dmit.domain;
+
+public class Circle {
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+//    public void setRadius(double radius) throws Exception {
+//        if(radius <= 0){
+//            throw new Exception("Radius must be greater than zero.");
+//        }
+//        this.radius = radius;
+//    }
+
+    public void setRadius(double radius) {
+        if(radius <= 0){
+            throw new RuntimeException("Radius must be greater than zero.");
+        }
+        this.radius = radius;
+    }
+
+}
